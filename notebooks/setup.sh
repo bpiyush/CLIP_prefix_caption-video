e859056d985738f11d4ae1751647829d716ad826
Piyush Bagad <piyushnbagad11@gmail.com>
2022-03-31 16:22:57 +0200

    Minor

---
+++ notebooks/setup.sh
@@ -0,0 +1,8 @@
+conda env create -n clip -f /path/to/repo/environment.yml
+conda activate clip
+pip install jupyterlab ipdb gdown tqdm tqdm-notebook 
+conda install -c conda-forge ipywidgets
+pip install ipywidgets
+pip install fiftyone
+pip install av
+
